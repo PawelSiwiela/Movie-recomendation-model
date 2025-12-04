427ee46d4b685f8ff0e203ea61dfdfcd6baa3cd7
PaeSielawa <siwielapawel@student.agh.edu.pl>
2025-12-04 12:54:58 +0100

    .gitignore update

--- src/model/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Moduł modelu rekomendacji filmów.
-
-Ten folder zawiera:
-- model.py: Architektura sieci neuronowej (PyTorch)
-- training.py: Proces trenowania modelu
-- recommender.py: System rekomendacji wykorzystujący wytrenowany model
-- utils.py: Funkcje pomocnicze (normalizacja, feature engineering)
-"""
